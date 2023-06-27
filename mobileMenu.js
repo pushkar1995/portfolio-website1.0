8f289ab92f660d35af5e8e733977b5e5e17706d4
pushkar1995 <pushkar.gautam1994@gmail.com>
2023-06-27 15:06:23 +0545

    Fix ESlint errors

--- mobileMenu.js
+++ mobileMenu.js
@@ -1,13 +1,13 @@
 const hamburgericon = document.querySelector('.fa-bars');
 const menulist = document.querySelector('.mobile-menu-list');
-const close = document.querySelector('.x-close-icon')
-const navItems = document.querySelectorAll('.mobile-menu-list ul li')
+const close = document.querySelector('.x-close-icon');
+const navItems = document.querySelectorAll('.mobile-menu-list ul li');
 
 const showMobileMenu = () => {
   hamburgericon.style.display = 'none';
   close.style.transform = 'translateY(0)';
   menulist.style.transform = 'translateY(0)';
-}
+};
 
 const hideMobileMenu = () => {
   menulist.style.transform = 'translateX(-100%)';
