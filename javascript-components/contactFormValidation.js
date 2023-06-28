ae2535258e162f0432f0688047891a95853651e0
pushkar1995 <pushkar.gautam1994@gmail.com>
2023-06-28 13:55:46 +0545

    Basic contact validation setup with JS

---
+++ javascript-components/contactFormValidation.js
@@ -0,0 +1,13 @@
+const contactForm = document.getElementById('contact-form');
+
+contactForm.addEventListener('submit', (event) => {
+  const emailInput = document.getElementById('email');
+  const emailValue = emailInput.value;
+  const lowercaseEmail = emailValue.toLowerCase();
+  if (lowercaseEmail !== emailValue) {
+    document.getElementById('error').innerHTML = '*Email must be in lowercase letters!';
+    event.preventDefault();
+    emailInput.value = '';
+    emailInput.focus();
+  }
+});
